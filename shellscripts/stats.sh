3c63323ee0273ef14de1e0dc132a64813a5b11c7
pradeep9030 <deepu.reddy609@gmail.com>
2019-05-08 13:33:05 +0000

    this is my first commit

---
+++ shellscripts/stats.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+echo "Hello, $LOGNAME" 
+echo "  $a " 
+echo " $b " 
+echo "  $c "
+a= 'date'
+b= 'who'
+c= 'pwd'
---
+++ shellscripts/testfor.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+for a in {1..10}
+do
+  if [ $a == 10 ]
+
+    then
+         Print the values
+        echo $a
+
+fi
+done
+
